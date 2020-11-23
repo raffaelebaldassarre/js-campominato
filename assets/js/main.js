912d17d2de690bfcc274800b6430272ce5e2d745
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2020-11-23 18:46:23 +0100

    50%

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,32 @@ con difficoltà 1 =>  tra 1 e 80
 con difficoltà 2 => tra 1 e 50 */
 
 
+// Il computer deve generare 16 numeri casuali tra 1 e 100.    
+// I numeri non possono essere duplicati
+var numberPc = [];
+
+while(numberPc.length<16){
+    numeriRandom = Math.floor(Math.random() * 100) + 1;
+    if(!(numberPc.includes(numeriRandom))){
+        numberPc.push(numeriRandom);
+    }
+}
+console.log(numberPc);
+
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+var numberPlayer = [];
+
+for(i=0; numberPlayer.length<3; i++){
+    number = Number(prompt("Inserisci un numero da 1 a 100"));
+    if(!(numberPlayer.includes(number))){
+        numberPlayer.push(number);
+    }else{
+        alert("Hai già inserito questo numero, per favore non farlo mai più");
+    }
+}
+console.log(numberPlayer);
+
+/* Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito. */
